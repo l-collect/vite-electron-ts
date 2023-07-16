669110614a097478d3ac5010f481492430996f5f
凉菜 <1659488338@qq.com>
2023-07-16 16:38:20 +0800

    add tsx demo

---
+++ src/views/JSX.tsx
@@ -0,0 +1,12 @@
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    render() {
+      return (
+          <>
+              <h1>This is page and drive by jsx</h1>
+              <h2>you can open `views/JSX.tsx` to visit source code</h2>
+          </>
+      )
+    }
+})
